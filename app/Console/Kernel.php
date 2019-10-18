0b5ec190b9e1c5e857513d74653a7d5c92aef99e
Vince Mitchell <vincent.mitchell@gmail.com>
2019-10-18 11:19:20 -0400

    increase cleanup frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,9 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('streams:cleanup')->everyMinute();
         $schedule->command('fetch:twitch')->everyFiveMinutes();
         // $schedule->command('fetch:youtube')->everyFiveMinutes();
-        $schedule->command('streams:cleanup')->everyFiveMinutes();
     }
 
     /**
